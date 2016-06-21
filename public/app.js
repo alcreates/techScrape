9493f50111695193e041d3ddbf058a013a663d13
Alvaro Martinez <alvaromartinez@Alvaros-MBP-2.home>
2016-06-21 10:52:33 -0400

    add tech scrape

---
+++ public/app.js
@@ -0,0 +1,56 @@
+$("#start").on('click', function(){
+$.getJSON('/articles', function(data) {
+  console.log(data);
+  for (var i = 0; i<data.length; i++){
+    $('#articles').append('<p data-id="' + data[i]._id + '">'+ data[i].title + '<br />'+ data[i].link + '</p>');
+  }
+});
+
+});
+
+
+
+
+$(document).on('click', 'p', function(){
+  $('#notes').empty();
+  var thisId = $(this).attr('data-id');
+
+  $.ajax({
+    method: "GET",
+    url: "/articles/" + thisId,
+  })
+    .done(function( data ) {
+      console.log("note data: " + data);
+      console.log("note data id: " + data._id)
+      $('#notes').append('<h2>' + data.title + '</h2>');
+      $('#notes').append('<input id="titleinput" name="title" >');
+      $('#notes').append('<textarea id="bodyinput" name="body"></textarea>');
+      $('#notes').append('<button data-id="' + data._id + '" id="savenote">Save Note</button>');
+
+      if(data.note){
+        $('#titleinput').val(data.note.title);
+        $('#bodyinput').val(data.note.body);
+      }
+    });
+});
+
+$(document).on('click', '#savenote', function(){
+  var thisId = $(this).attr('data-id');
+
+  $.ajax({
+    method: "POST",
+    url: "/articles/" + thisId,
+    data: {
+      title: $('#titleinput').val(),
+      body: $('#bodyinput').val()
+    }
+  })
+    .done(function( data ) {
+      console.log(data);
+      $('#notes').empty();
+    });
+
+
+  $('#titleinput').val("");
+  $('#bodyinput').val("");
+});
