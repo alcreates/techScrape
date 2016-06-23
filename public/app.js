7433f40ddc07ee4dfe15c833a85302d4685a196a
Alvaro Martinez <alvaromartinez@Alvaros-MBP-2.home>
2016-06-23 13:33:19 -0400

    link

--- public/app.js
+++ public/app.js
@@ -2,7 +2,7 @@ $("#start").on('click', function(){
 $.getJSON('/articles', function(data) {
   console.log(data);
   for (var i = 0; i<data.length; i++){
-    $('#articles').append('<p data-id="' + data[i]._id + '">'+ data[i].title + '<br />'+ '<a href="' + data[i].link '"></a>' + data[i].link + '</p>');
+    $('#articles').append('<p data-id="' + data[i]._id + '">'+ data[i].title + '<br />'+ '<a href="' + data[i].link '"></a>' + '</p>');
   }
 });
 
